483b5c00b2529fea02336f4461c5a6849a7ad7df
RJ <1547237+li-ruijie@users.noreply.github.com>
2025-11-26 14:55:07 +0800

    initial

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * AssemblyInfo.cs
+ *
+ * Purpose:
+ * This file contains assembly-level metadata for the ThemeTool executable.
+ * It defines the version number that will be embedded in the compiled binary.
+ *
+ * Assembly attributes are used by the .NET runtime and tools to identify
+ * the version and other properties of the compiled assembly.
+ */
+
+using System.Reflection;
+
+// Assembly Version: Specifies the version number of this assembly
+// Format: Major.Minor.Build.Revision
+// Version 0.0.0.0 indicates this is a development/prototype build
+[assembly: AssemblyVersion("0.0.0.0")]
